e3c86eab1f494c47c397bff8cfe9877192224e09
jfernandezro <jfernandezro@ucenfotec.ac.cr>
2026-08-15 00:50:32 +0000

    add dast

--- src/main.js
+++ src/main.js
@@ -86,7 +86,7 @@ router.get('/search', (req, res) => {
   const query = req.query.q;
 
   // Vulnerable rendering
-  res.send(`<h1>Search Results for: ${query}</h1>`);
+  res.send({ query: query });
 });
 
 app.use(router)
